60f8b463ecc5dded692fd8808508c670d43f63cb
powwu <hello@powwu.sh>
2025-07-08 20:43:23 -0700

    ,

--- download-repos.sh
+++ download-repos.sh
@@ -5,7 +5,7 @@ read -p "Upload location (s3://my-bucket/folder-navigation/): " LOC < /dev/tty
 
 echo "Downloading repos"
 mkdir -p /tmp/github-$(date --iso-8601=s) && cd $_
-gh repo list --limit 2 "$USER" --json nameWithOwner -q '.[].nameWithOwner' | xargs -L1 gh repo clone
+gh repo list "$USER" --json nameWithOwner -q '.[].nameWithOwner' | xargs -L1 gh repo clone
 find . -mindepth 1 -maxdepth 1 -type d -exec 7z a -mx=9 {}.7z {} \;
 find . -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \; # would usually use && but find is picky
 echo "Copying to S3"
